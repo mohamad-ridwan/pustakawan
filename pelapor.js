892b008bc204c83088183484b58ee8506a6f5fef
mohamad-ridwan-2 <ridwan.reactjs@gmail.com>
2023-09-05 17:32:30 +0700

    commit

--- pelapor.js
+++ pelapor.js
@@ -18,6 +18,7 @@
 
 function recaptchaCallback(){
     console.log('callback')
+    console.log('recaptcha-response',grecaptcha.getResponse())
 }
 
 // document.getElementById('formPengajuan').addEventListener('submit', (e)=>{
@@ -26,5 +27,19 @@ function recaptchaCallback(){
 // })
 
 function submit(){
-    console.log(grecaptcha.getResponse())
+    console.log('submit is clicked')
+}
+
+function isCaptchaChecked(){
+    return grecaptcha && grecaptcha.getResponse().length !== 0
+}
+
+const btnSubmit = document.getElementById('submit')
+
+if(isCaptchaChecked() && btnSubmit){
+    btnSubmit.style.cursor = 'not-allowed'
+}else{
+    if(btnSubmit){
+        btnSubmit.style.cursor = 'pointer'
+    }
 }
