4feea152662571b9ce2fc812ad46945f1f9034bf
mohamad-ridwan-2 <ridwan.reactjs@gmail.com>
2023-09-05 14:35:05 +0700

    commit

---
+++ pelapor.js
@@ -0,0 +1,22 @@
+const verifyCallback = (res)=>{
+    console.log(res)
+}
+
+const sitekey = '6LeAY_4nAAAAAG4mEOA8_sXdkoXMSHb7MKLoqcgh'
+
+let captcha
+const onloadCallback = ()=>{
+    captcha = grecaptcha.render('recaptcha-pustakawan', {
+        'sitekey' : sitekey,
+        'callback': verifyCallback()
+    })
+}
+
+// document.getElementById('formPengajuan').addEventListener('submit', (e)=>{
+//     console.log(grecaptcha.getResponse(captcha))
+//     e.preventDefault()
+// })
+
+function submit(){
+    console.log(grecaptcha.getResponse(captcha))
+}
