e532f91c1fd3ab4e2cdfba75cf4561655ffd6f93
mohamad-ridwan-2 <ridwan.reactjs@gmail.com>
2023-09-05 18:18:54 +0700

    commit

--- pelapor.js
+++ pelapor.js
@@ -1,43 +1,94 @@
-// const verifyCallback = (res)=>{
-//     console.log(res)
-// }
-
-// const sitekey = '6LeAY_4nAAAAAG4mEOA8_sXdkoXMSHb7MKLoqcgh'
-
-// let captcha
-// const onloadCallback = ()=>{
-//     captcha = grecaptcha.render('recaptcha-pustakawan', {
-//         'sitekey' : sitekey,
-//         'callback': verifyCallback()
-//     })
-//     grecaptcha.render('recaptcha-pustakawan', {
-//         'sitekey' : sitekey,
-//         'callback': verifyCallback()
-//     })
-// }
-
 function recaptchaCallback() {
-    return grecaptcha && grecaptcha.getResponse().length > 0
+    const check = grecaptcha && grecaptcha.getResponse().length > 0
+    if(check){
+        document.getElementById('btnSubmitForm').style.cursor = 'pointer'
+    }
 }
 
 function recaptchaExpiredCallback() {
-    console.log('recaptcha is expired')
+    document.getElementById('btnSubmitForm').style.cursor = 'not-allowed'
 }
 
 function isCaptchaChecked() {
     return grecaptcha && grecaptcha.getResponse().length !== 0
 }
 
+window.onload = ()=>{
+    document.getElementById('btnSubmitForm').style.cursor = 'not-allowed'
+}
+
+const header = {
+    "alg": "HS256",
+    "typ": "JWT"
+};
+
+// create jwt
+function createJWTToken(
+    header,
+    data,
+    secret = undefined
+){
+    var encodedSource
+    function base64url(source){
+        encodedSource = CryptoJS.enc.Base64.stringify(source)
+        encodedSource = encodedSource.replace(/=+$/, '')
+        encodedSource = encodedSource.replace(/\+/g, '')
+        encodedSource = encodedSource.replace(/\//g, '')
+        return encodedSource
+    }
+
+    function createJWT(
+        header,
+        data,
+        secret = undefined
+    ){
+        let stringifiedHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header))
+        let encodedHeader = base64url(stringifiedHeader)
+        let stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(data))
+        let encodedData = base64url(stringifiedData)
+        let token = encodedHeader + '.' + encodedData
+        if(!secret)return token
+        let signature = CryptoJS.HmacSHA256(token, secret)
+        signature = base64url(signature)
+        return encodedHeader + '.' + encodedData + '.' + signature
+    }
+    const secrets = "john@99";
+    // jwt result
+    const token = createJWT(header, data, secrets)
+    return token
+}
+
+function changeInput(nameInput){
+    const value = document.getElementById(nameInput).value
+    return value
+}
+
+function submitForm(){
+    let data = {}
+    data.name = changeInput('nama')
+    data.email = changeInput('email')
+    data.telpon = changeInput('telepon')
+    const createTime = new Date()
+    createTime.setHours(7)
+    data.time = `${createTime}`
+    return data
+}
+
+// submit
+// document.getElementById('formPengajuan').addEventListener('click', (e)=>{
+//     window.open(`pilih-menu.html?akses=${createJWTToken(
+//         header,
+//         submitForm()
+//     )}`)
+// })
+
 function submit() {
     if(isCaptchaChecked()){
-        window.open('pilih-menu.html')
+        window.open(`pilih-menu.html?akses=${createJWTToken(
+            header,
+            submitForm()
+        )}`)
     }else{
         console.log('please check the checkbox')
     }
-}
-
-if (recaptchaCallback()) {
-    document.getElementById('submit').style.cursor = 'pointer'
-} else {
-    document.getElementById('submit').style.cursor = 'not-allowed'
 }
