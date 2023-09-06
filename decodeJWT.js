1cc9b8b51bdab937552e966525fea1aec00673a4
mohamad-ridwan-2 <ridwan.reactjs@gmail.com>
2023-09-06 20:30:52 +0700

    commit

--- decodeJWT.js
+++ decodeJWT.js
@@ -1,4 +1,4 @@
-const header = {
+const headerJwt = {
     "alg": "HS256",
     "typ": "JWT"
 };
@@ -21,7 +21,7 @@ const parseTokenUrl = async (token) => {
 }
 
 // decode from token url
-function decodeFromTokenUrl() {
+async function decodeFromTokenUrl() {
     const urlParams = new URLSearchParams(window.location.search)
     const token = urlParams.get('akses')
     if (!token) {
@@ -29,34 +29,29 @@ function decodeFromTokenUrl() {
         return
     }
     // payload from token url
-    let newToken = {}
     const getToken = token?.includes('.') ? window.atob(token.split('.')[1]) : window.atob(token)
-    parseTokenUrl(getToken)
-        .then(res => {
-            newToken.payload = res
-        })
-    const wordArrayHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header))
+    const base64HeaderUrl = token?.includes('.') ? window.atob(token.split('.')[0]) : window.atob(token)
+
+    const resultObjTokenUrl = await parseTokenUrl(getToken).then(res => res)
+    const headerJwtFromUrl = JSON.parse(base64HeaderUrl)
+    const wordArrayHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(headerJwtFromUrl))
     const base64Header = CryptoJS.enc.Base64.stringify(wordArrayHeader)
 
-    const wordArrayPayload = CryptoJS.enc.Utf8.parse(JSON.stringify(newToken.payload))
+    const wordArrayPayload = CryptoJS.enc.Utf8.parse(JSON.stringify(resultObjTokenUrl))
     const base64Payload = CryptoJS.enc.Base64.stringify(wordArrayPayload)
     const signatueToStr = CryptoJS.HmacSHA256(base64Header + '.' + base64Payload, secrets).toString(CryptoJS.enc.Base64)
-    return signatueToStr
+    return {signature: signatueToStr, time: resultObjTokenUrl.time}
 }
 
-function decodeFromTokenLocalStg() {
+async function decodeFromTokenLocalStg() {
     // local storage
-    let payloadFromTokenStg = {}
     const aksesClientP = localStorage.getItem('akses-client-p')
     if (!aksesClientP) {
         redirectToPelapor()
         return
     }
     const getTokenLocalStg = aksesClientP?.includes('.') ? window.atob(aksesClientP.split('.')[1]) : window.atob(aksesClientP)
-    parseTokenUrl(getTokenLocalStg)
-        .then(res => {
-            payloadFromTokenStg.payload = res
-        })
+    const resultObjTokenLocalStg = await parseTokenUrl(getTokenLocalStg).then(res => res)
     // header jwt
     // from local storage
     const headerFromLocalStg = JSON.parse(window.atob(aksesClientP.split('.')[0]))
@@ -64,26 +59,42 @@ function decodeFromTokenLocalStg() {
     const base64HeaderFromLocalStg = CryptoJS.enc.Base64.stringify(wordArrayHeaderLocalStg)
 
     // base64 payload from local storage
-    const wordArrayPayloadLocalStg = CryptoJS.enc.Utf8.parse(JSON.stringify(payloadFromTokenStg.payload))
+    const wordArrayPayloadLocalStg = CryptoJS.enc.Utf8.parse(JSON.stringify(resultObjTokenLocalStg))
     const base64PayloadLocalStg = CryptoJS.enc.Base64.stringify(wordArrayPayloadLocalStg)
 
     // signature from local storage
     const signatureLocalStgToStr = CryptoJS.HmacSHA256(base64HeaderFromLocalStg + '.' + base64PayloadLocalStg, secrets).toString(CryptoJS.enc.Base64)
-    return signatureLocalStgToStr
+    return {signature: signatureLocalStgToStr, time: resultObjTokenLocalStg.time}
 }
 
-function jwtTokenVerify() {    
-    const isValid = decodeFromTokenUrl() == decodeFromTokenLocalStg()
-    if (isValid) {
-        return true
-    }
-    return false
+async function jwtTokenVerify() {
+    // const isValid = decodeFromTokenUrl() == decodeFromTokenLocalStg()
+    // if (isValid) {
+    //     return true
+    // }
+    // return false
+    return Promise.all([decodeFromTokenUrl(), decodeFromTokenLocalStg()]).then(res=>{
+        const time = new Date(res[1].time).valueOf()
+        const validTime = new Date().valueOf() < time
+        const validToken = res[0].signature == res[1].signature && validTime
+
+        return validToken
+    })
 }
 
 window.onload = () => {
-    if (jwtTokenVerify()) {
-        console.log('masuk')
-    } else {
-        redirectToPelapor()
-    }
+    jwtTokenVerify()
+    .then(res=>{
+        if(res){
+            console.log('masuk')
+        }else{
+            redirectToPelapor()
+        }
+    })
+    .catch(err=>redirectToPelapor())
+    // if (jwtTokenVerify()) {
+    //     console.log('masuk')
+    // } else {
+    //     redirectToPelapor()
+    // }
 }
