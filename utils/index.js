88a621dabc22158c15e035e497bbdff7f2b2b9e1
mohamad-ridwan-2 <ridwan.reactjs@gmail.com>
2023-10-05 09:32:32 +0700

    commit

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,3 @@
 window.onload = ()=>{
-    window.location.replace('/pages/pilih-menu/pilih-menu.html')
+    window.location.replace('/pages/pelapor-pengajuan-mandiri/pelapor-pengajuan-mandiri.html')
 }
